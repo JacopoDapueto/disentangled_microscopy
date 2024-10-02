7bfb94ff35f77f9b88a641d3100b0879ce307cf2
Jacopo Dapueto <jacopo.dapueto@gmail.com>
2024-10-02 11:55:02 +0200

    disentanglement score

---
+++ code/dataset/utils.py
@@ -0,0 +1,16 @@
+import os
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple, Union
+
+
+def find_classes(directory: str, not_drop_classes) -> Tuple[List[str], Dict[str, int]]:
+    """Finds the class folders in a dataset.
+
+    See :class:`DatasetFolder` for details.
+    """
+    classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+    classes = [c for c in classes if c in not_drop_classes]
+    if not classes:
+        raise FileNotFoundError(f"Couldn't find any class folder in {directory}.")
+
+    class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+    return classes, class_to_idx
